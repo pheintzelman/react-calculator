a38a99e452391c73719d71b78037563af32d9be6
Paul Heintzelman <pheintzelman@gmail.com>
2017-04-23 15:45:50 -0700

    fix path issue in demo

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -16,7 +16,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/docs/'),
         filename: '[name]-[hash].min.js',
-        publicPath: '/'
+        publicPath: ''
     },
     plugins: [
         new CopyWebpackPlugin([
